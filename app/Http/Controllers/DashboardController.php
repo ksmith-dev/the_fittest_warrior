8f03050264f05b324c99cc547903ceb34f885a3d
Kevin Smith <kjsmith.dev@gmail.com>
2018-03-31 22:55:55 -0700

    travis ci work

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,10 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use App;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Unit;
+use App;
+
+use App\User;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    public function testUserInformation() {
+
+    }
+}
