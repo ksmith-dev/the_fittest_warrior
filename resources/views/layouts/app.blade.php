86f8822eae7bc537f02911edfc9117e7c721f1b8
Dan Capps <dcapps@mail.greenriver.edu>
2018-04-13 21:41:23 -0700

    Added Google analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,17 @@
 <html lang="{{ app()->getLocale() }}">
 <head>
     @section('head')
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-117553531-1"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-117553531-1');
+        </script>
+
+        <!-- Start of Head -->
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
