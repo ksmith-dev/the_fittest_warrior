f041631f155d7c460f898bb57b01a3c54b6e3abf
dmitriy012 <dmitriyarkhipchuk@yahoo.com>
2018-05-23 19:50:48 -0700

    HTML file that holds the contact us info

---
+++ resources/views/email/contact_us.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+
+<h1>Someone Contacted You</h1>
+
+<p>First Name: {{ $firstName }}</p>
+<p>Last Name: {{ $lastName }}</p>
+<p>Email: {{ $email }}</p>
+<p>Comment</p>
+<p>{{ $comment}}</p>
+
+</body>
+</html>
